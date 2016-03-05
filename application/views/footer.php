78d535805ab00d83aed260c5c9d7ea0056b700cc
tusharm-cuelogic <tushar.mate@cuelogic.co.in>
2016-03-05 22:18:57 +0530

    added boostrap css and js

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,5 +1,10 @@
-<div>
-    <footer>
-        footer
-    </footer>
+
 </div>
+
+	<hr>
+	<footer>
+	    <p class="pull-right"><a href="#">Copyright © 2016 Project Police</a></p>
+	</footer>
+	</div>
+</body>
+</html>
--- application/views/header.php
+++ application/views/header.php
@@ -1,5 +1,46 @@
-<h1>
-    <HEADER>
-        LOGO
-    </HEADER>
-</h1>
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-COMPATIBLE" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<title>
+		Project Police
+	</title>
+	
+	<link rel="stylesheet" href="../../assets/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="../../assets/css/bootstrap-theme.min.css">
+	<link rel="stylesheet" type="text/css" href="../../assets/css/theme.css">
+	
+	<script src="../../assets/js/jquery.min.js"></script>
+	<script src="../../assets/js/bootstrap.min.js"></script>
+</head>
+<body role="document">
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Home</a>
+            </div>
+            
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+						<a href="#">Login</a>
+					</li>
+					<li>
+						<a href="#">Signup</a>
+					</li>
+				</ul>
+            </div>
+            
+        </div>
+    </div>
+    <div class="container theme-showcase" role="main">
